cb6bf98ea9f17df322aeacc578f91bf5ccf2021e
alessandrodaniello105 <kanymsevement@gmail.com>
2023-10-19 14:57:39 +0200

    [add]: vite/vue/sass + Hello World

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<script>
+import Hello from './components/Hello.vue';
+
+export default {
+  name: 'App',
+  components: {
+    Hello
+  }
+}
+</script>
+
+<template>
+  <div>
+    <h1>APP</h1>
+
+    <Hello />
+
+  </div>
+</template>
+
+<style>
+
+</style>
---
+++ src/components/Hello.vue
@@ -0,0 +1,13 @@
+<script>
+export default {
+  name: 'Hello'
+}
+</script>
+
+<template>
+  <h2>Hello World!</h2>
+</template>
+
+<style>
+
+</style>
