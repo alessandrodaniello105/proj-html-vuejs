e2720e3512175b8a779fb31326fccacbddfae386
alessandrodaniello105 <kanymsevement@gmail.com>
2023-10-21 12:39:06 +0200

    [add]: header menu info as data

---
+++ src/data/headerMenu.js
@@ -0,0 +1,35 @@
+export default [
+  {
+    text: 'Home',
+    href: '#',
+    isActive: true
+  },
+  
+  {
+    text: 'About',
+    href: '#',
+    isActive: false
+  },
+  {
+    text: 'Services',
+    href: '#',
+    isActive: false
+  },
+  {
+    text: 'Showcase',
+    href: '#',
+    isActive: false
+  },
+  {
+    text: 'Blog',
+    href: '#',
+    isActive: false
+  },
+  {
+    text: 'Contact',
+    href: '#',
+    isActive: false
+  }
+
+]
+
