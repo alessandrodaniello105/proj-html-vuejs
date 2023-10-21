3799e4419ac846f3b0bc70e925c614c80b986e4d
alessandrodaniello105 <kanymsevement@gmail.com>
2023-10-21 20:29:26 +0200

    [add]: Phlox lorem in store

--- src/data/store.js
+++ src/data/store.js
@@ -1,5 +1,9 @@
 import {reactive} from 'vue';
 
 export const store = reactive ({
-  isActive: {}
+  isActive: {},
+  getUpperCase(text) {
+    text = text.toUpperCase();
+  },
+  lorem: 'Far far away, behind the the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. Separated they live in Bookmarksgrove right at the coast of the Semantics, a large language ocean. Separated they live in Bookmarksgrove.'
 })
