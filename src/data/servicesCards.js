1f044139d7c3f594a219911d85cadbfa50f8f41a
alessandrodaniello105 <kanymsevement@gmail.com>
2023-10-21 20:29:07 +0200

    [add]: services cards data in servicesCards.js

---
+++ src/data/servicesCards.js
@@ -0,0 +1,22 @@
+export default [
+  {
+    title: 'Speed Optimization',
+    text: 'Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. Separated they live in Bookmarksgrove right at the coast of the Semantics, a large language ocean. Separated they live in Bookmarksgrove.',
+    src: 'optimization.svg',
+  },
+  {
+    title: 'Cloud Solutions',
+    text: 'Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. Separated they live in Bookmarksgrove right at the coast of the Semantics, a large language ocean. Separated they live in Bookmarksgrove.',
+    src: 'optimization.svg',
+  },
+  {
+    title: 'Website Design',
+    text: 'Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. Separated they live in Bookmarksgrove right at the coast of the Semantics, a large language ocean. Separated they live in Bookmarksgrove.',
+    src: 'optimization.svg',
+  },
+  {
+    title: 'Online Marketing',
+    text: 'Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. Separated they live in Bookmarksgrove right at the coast of the Semantics, a large language ocean. Separated they live in Bookmarksgrove.',
+    src: 'optimization.svg',
+  }
+]
