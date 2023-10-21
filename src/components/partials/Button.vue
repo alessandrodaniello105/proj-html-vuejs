336096d93b3648162f346e13857c9108c4498d8d
alessandrodaniello105 <kanymsevement@gmail.com>
2023-10-21 12:38:11 +0200

    [add]: Button component

---
+++ src/components/partials/Button.vue
@@ -0,0 +1,17 @@
+<script>
+export default {
+  name: 'Button',
+  props: {
+    text: String,
+    green: String
+  }
+}
+</script>
+
+<template>
+  <div :class="green" class="btn_ctm">{{ text }}</div>
+</template>
+
+<style>
+
+</style>
