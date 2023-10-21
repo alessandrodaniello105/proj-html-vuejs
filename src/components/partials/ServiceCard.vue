a2e6b6443a8f191b19732c7515d11878d4b5e926
alessandrodaniello105 <kanymsevement@gmail.com>
2023-10-21 20:29:42 +0200

    [add]: services card as component

---
+++ src/components/partials/ServiceCard.vue
@@ -0,0 +1,53 @@
+<script>
+// DATA
+import servicesCards from '../../data/servicesCards';
+
+export default {
+  name: 'ServiceCard',
+  props: {
+    card: Object
+  },
+  data() {
+    return {
+      servicesCards
+    }
+  },
+}
+</script>
+
+<template>
+       
+  <div class="card service">
+
+    <div class="img-box">
+      <img :src="`/src/assets/img/${card.src}`" :alt="`${card.title} icon`">
+    </div>
+
+    <div class="text-box">
+      <h3>{{ card.title }}</h3>
+      <div class="special-text"><p>{{ card.text }}</p></div>
+    </div>
+
+  </div>
+
+</template>
+
+<style lang="scss">
+.special-text p {
+  display: -webkit-box;
+  -webkit-line-clamp: 2;
+  -webkit-box-orient: vertical;
+  overflow: hidden;
+}
+
+.card.service {
+  margin: 30px 0;
+  margin-left: 40px;
+  padding: 40px;
+  box-shadow: 0 0 20px rgba(0, 0, 0, .2);
+  .img-box {
+    width: 75px;
+  }
+
+}
+</style>
