845b761a98328910bde11108d9d010af96704da4
alessandrodaniello105 <kanymsevement@gmail.com>
2023-10-22 18:20:43 +0200

    [add]: Blog component

---
+++ src/components/Blog.vue
@@ -0,0 +1,79 @@
+<script>
+import posts from '../data/posts';
+import Divider from './partials/Divider.vue';
+import SwiperT from './partials/SwiperT.vue';
+import Button from './partials/Button.vue';
+
+
+
+export default {
+  name: 'Blog',
+  components: {
+    Divider,
+    SwiperT,
+    Button
+  },
+  data() {
+    return {
+      posts
+    }
+  },
+}
+</script>
+
+<template>
+<div class="container  d-flex-column">
+  <div class="text-box">
+
+    <h2 class="title">Latest News & Our <span class="text-green">Blog</span></h2>
+
+    <Divider />
+
+  </div>
+
+  <div class="posts-wrapper">
+    <div class="container">
+
+      <SwiperT :list="posts" />
+
+    </div>
+  </div>
+
+  <Button :text="'VIEW ALL POST'" :type="'black'" />
+
+</div>
+
+  
+</template>
+
+<style lang="scss">
+.container {
+  text-align: center;
+  overflow: hidden;
+  width: 100%;
+  height: 100%;
+  //max-width: none !important;
+  .text-box {
+    margin-top: 240px;
+    .divider {
+      padding-top: 30px;
+      width: fit-content;
+      margin: 0 auto;
+    }
+  }
+}
+
+.posts-wrapper {
+  width: 1800px;
+}
+
+.btn_ctm {
+  margin: 50px 0 200px;
+}
+
+//  .posts-wrapper {
+//    .container {
+//      width: 100vw !important;
+//    }
+//  }
+</style>
