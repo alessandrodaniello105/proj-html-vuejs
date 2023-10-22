80fefd7501e29f56d4d1a4ed523d6e02dc33b3cd
alessandrodaniello105 <kanymsevement@gmail.com>
2023-10-22 20:28:57 +0200

    [add]: Contact as component

---
+++ src/components/Contact.vue
@@ -0,0 +1,22 @@
+<script>
+import SpecialBox from './partials/SpecialBox.vue'
+import Button from './partials/Button.vue';
+
+export default {
+  name:'Contact',
+  components: {
+    SpecialBox,
+    Button
+  }
+}
+</script>
+
+<template>
+  <div class="container">
+    <SpecialBox />
+  </div>
+</template>
+
+<style lang="scss">
+
+</style>
