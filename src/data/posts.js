5ee81200a9fc5bca28fa55dcf28b4bdee0819b76
alessandrodaniello105 <kanymsevement@gmail.com>
2023-10-22 18:20:28 +0200

    [add]: posts.js in \data\

---
+++ src/data/posts.js
@@ -0,0 +1,38 @@
+export default [
+  {
+    date: 'May 24, 2018',
+    author: 'Alex',
+    title: 'Marketing Ideas',
+    image: 'serious-businesswoman-with-documents-talking-on-P9Q6LX6-1024.jpg'
+  },
+  {
+    date: 'May 24, 2018',
+    author: 'Alex',
+    title: 'Rest During Working Hours',
+    image: 'Businessman-at-the-desk-in-his-office-resting--1024x768.jpg'
+  },
+  {
+    date: 'May 24, 2018',
+    author: 'Alex',
+    title: 'Develop Your Startup Idea',
+    image: 'simple-home-office-with-tree-PBXRXYB-large-1024x768.jpg'
+  },
+  {
+    date: 'May 24, 2018',
+    author: 'Alex',
+    title: 'Marketing Ideas',
+    image: 'serious-businesswoman-with-documents-talking-on-P9Q6LX6-1024.jpg'
+  },
+  {
+    date: 'May 24, 2018',
+    author: 'Alex',
+    title: 'Rest During Working Hours',
+    image: 'Businessman-at-the-desk-in-his-office-resting--1024x768.jpg'
+  },
+  {
+    date: 'May 24, 2018',
+    author: 'Alex',
+    title: 'Develop Your Startup Idea',
+    image: 'simple-home-office-with-tree-PBXRXYB-large-1024x768.jpg'
+  }
+]
