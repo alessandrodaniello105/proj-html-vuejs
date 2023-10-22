29b46c780e33b43ae79209eca45cfa200092c3e6
alessandrodaniello105 <kanymsevement@gmail.com>
2023-10-22 20:31:24 +0200

    [add]: contactCards, customers in \data\

---
+++ src/data/contactCards.js
@@ -0,0 +1,14 @@
+export default [
+  {
+    type: 'black',
+    question: 'Are You Ready?',
+    title: 'Start a New Project',
+    input: 'email'
+  },
+  {
+    type: 'green',
+    question: 'What Are You Waiting for?',
+    title: 'Let\'s Talk About Work',
+    input: 'button'
+  }
+]
---
+++ src/data/customers.js
@@ -0,0 +1,26 @@
+export default [
+  {
+    customer: 'Avantgarde',
+    src: 'client-1-2x'
+  },
+  {
+    customer: 'Fastlane',
+    src: 'client-9-2x'
+  },
+  {
+    customer: 'Ron Jones',
+    src: 'client-7-2x'
+  },
+  {
+    customer: 'Chippy\'s',
+    src: 'client-3-2x'
+  },
+  {
+    customer: 'Bullseye',
+    src: 'client-4-2x'
+  },
+  {
+    customer: 'Mighty Furnitures',
+    src: 'client-5-2x'
+  }
+]
