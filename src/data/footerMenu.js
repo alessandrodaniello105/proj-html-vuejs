fe347005426aecaba739bcf4607569de251b97f5
alessandrodaniello105 <kanymsevement@gmail.com>
2023-10-23 16:19:33 +0200

    [add]: footerMenu in \data\

---
+++ src/data/footerMenu.js
@@ -0,0 +1,26 @@
+export default [
+  {
+    text: 'Careers',
+    href: '#'
+  },
+  {
+    text: 'News',
+    href: '#'
+  },
+  {
+    text: 'Terms of use',
+    href: '#'
+  },
+  {
+    text: 'Privacy Projects',
+    href: '#'
+  },
+  {
+    text: 'About',
+    href: '#'
+  },
+  {
+    text: 'Contact',
+    href: '#'
+  }
+]
