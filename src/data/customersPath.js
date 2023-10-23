bec83ab3d61efa7e0653ee019b42064438069cb8
alessandrodaniello105 <kanymsevement@gmail.com>
2023-10-23 13:04:16 +0200

    [edit]: renamed 'customers' as 'customersPath'

--- src/data/customersPath.js
+++ src/data/customersPath.js
@@ -1,26 +1,26 @@
 export default [
   {
     customer: 'Avantgarde',
-    src: 'client-1-2x'
+    src: 'client-1-2x.png'
   },
   {
     customer: 'Fastlane',
-    src: 'client-9-2x'
+    src: 'client-9-2x.png'
   },
   {
     customer: 'Ron Jones',
-    src: 'client-7-2x'
+    src: 'client-7-2x.png'
   },
   {
     customer: 'Chippy\'s',
-    src: 'client-3-2x'
+    src: 'client-3-2x.png'
   },
   {
     customer: 'Bullseye',
-    src: 'client-4-2x'
+    src: 'client-4-2x.png'
   },
   {
     customer: 'Mighty Furnitures',
-    src: 'client-5-2x'
+    src: 'client-5-2x.png'
   }
 ]
