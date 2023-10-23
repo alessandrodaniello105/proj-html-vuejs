275c39a74819242aee2d82f9f6478ea1adc569de
alessandrodaniello105 <kanymsevement@gmail.com>
2023-10-23 20:08:46 +0200

    [edit] speed optimization icon

--- src/data/servicesCards.js
+++ src/data/servicesCards.js
@@ -2,7 +2,7 @@ export default [
   {
     title: 'Speed Optimization',
     text: 'Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. Separated they live in Bookmarksgrove right at the coast of the Semantics, a large language ocean. Separated they live in Bookmarksgrove.',
-    src: 'zmdi-flower-alt',
+    src: 'zmdi-plus-circle',
   },
   {
     title: 'Cloud Solutions',
