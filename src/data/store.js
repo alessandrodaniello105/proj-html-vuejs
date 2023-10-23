8d4dd55ae38e8cc48e165a4b63ada663abac941b
alessandrodaniello105 <kanymsevement@gmail.com>
2023-10-23 13:04:36 +0200

    [add]: getImagePath() to store

--- src/data/store.js
+++ src/data/store.js
@@ -3,7 +3,10 @@ import {reactive} from 'vue';
 export const store = reactive ({
   isActive: {},
   getUpperCase(text) {
-    text = text.toUpperCase();
+    return text = text.toUpperCase();
+  },
+  getImagePath(path) {
+    return new URL(`/src/assets/img/${path}`, import.meta.url).href;
   },
   lorem: 'Far far away, behind the the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. Separated they live in Bookmarksgrove right at the coast of the Semantics, a large language ocean. Separated they live in Bookmarksgrove.'
 })
